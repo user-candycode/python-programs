105173253e8886e194afa10ab61c8c562099b945
candy <user.candycode@gmail.com>
2023-04-25 00:07:04 +0530

    half way on loops

---
+++ first/seventh/01_while_loops.py
@@ -0,0 +1,5 @@
+i=1
+print("type of i is:",type(i))
+while i<10:
+    print(i)
+    i+=1
---
+++ first/seventh/02_print_list__using_while.py
@@ -0,0 +1,13 @@
+print("\n=======using while loop=======\n")
+fruits=["Banana","Mango","Pineapple","Pomigranate"]
+i=0
+while i<len(fruits):
+    print(fruits[i])
+    i+=1
+
+#using for loop
+print("\n=======using for loop=======\n")
+fruits=["Banana","Mango","Pineapple","Pomigranate"]
+
+for a in fruits:
+    print(a)
