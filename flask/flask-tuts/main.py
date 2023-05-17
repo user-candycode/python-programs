ef5fd9129abb9ede78bd13e6feecc0a6e878bb06
candy <user.candycode@gmail.com>
2023-05-17 21:22:48 +0530

    learned to connect mysql to flask app

--- flask/flask-tuts/main.py
+++ flask/flask-tuts/main.py
@@ -1,6 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+import mysql.connector
+import json
+
+from datetime import datetime
+
+with open('config.json','r') as c:
+    params = json.load(c)["parameters"]
+local_server = True
 
 app = Flask(__name__)
+if(local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] = params["local_uri"]
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params["prod_uri"]
+
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    phone_num = db.Column(db.String(12), nullable=False)
+    msg = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(12), nullable=True)
+    email = db.Column(db.String(20), nullable=False)
 
 
 @app.route("/")
@@ -13,8 +38,21 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        '''ADD entry to the database'''
+        name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        message = request.form.get('message')
+
+        entry = Contacts(name=name, phone_num=phone, msg=message,
+                         date=datetime.datetime.now(), email=email)
+
+        db.session.add(entry)
+        db.session.commit()
+
     return render_template("contact.html")
 
 
@@ -23,4 +61,5 @@ def post():
     return render_template("post.html")
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
