bf5137d9613fef25cc852e8fbb85e9295b563ffe
user-candycode <user.candycode@gmail.com>
2023-09-15 19:58:49 +0530

    random

---
+++ restart/basics/random/01_script.py
@@ -0,0 +1,1 @@
+print(f"Hello my name is {__name__},my file is {__file__}")
---
+++ restart/basics/random/02_not_a_script.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def hello():
+	print(f"Hello, My name is {__name__}, my file name is {__file__}")
+
+if __name__ == "__main__":
+	hello()
