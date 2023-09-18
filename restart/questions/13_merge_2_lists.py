56bb85e6cfdee4412b435598c9efcf08e32886d9
candy <user.candycode@gmail.com>
2023-09-18 21:46:44 +0530

    some more practice questions

---
+++ restart/questions/13_merge_2_lists.py
@@ -0,0 +1,7 @@
+lang1 = ['marathi',"hindi",'gujarati',"urdu"]
+lang2 = ['english','french','japanese']
+
+for l in lang2:
+    lang1.append(l)
+
+print(lang1)
---
+++ restart/questions/14_extend_function_merge_2_list.py
@@ -0,0 +1,6 @@
+list1 = ['marathi',"hindi",'gujarati',"urdu"]
+list2 = ['english','french','japanese']
+
+list1.extend(list2)
+
+print(list1)
