53b024ecd5bc582bad9c7b2ac7bd756ce9f50c9f
candy <user.candycode@gmail.com>
2023-11-26 19:50:19 +0530

    started polygon calculator

--- project/budget app/test.py
+++ project/budget app/test.py
@@ -1,26 +1,138 @@
-cateogaries = ['Food','Auto','Clothing']
-chart = "Percentage spent by category\n"
-percentage = [10,70,30]
+class Category:
 
-height = (len(max(cateogaries, key=len)))
-padded = [names.ljust(height) for names in cateogaries]
-# for names in cateogaries:
-#     padded.append(names.ljust(height))
 
-for i in reversed(range(0,110,10)):
-    chart += f"{str(i) + '|':>4}"
-    for percent in percentage:
-        if percent>=i:
-            chart += " o "
-        else:
-            chart+="   "
-    chart += '\n'
+  def __init__(self, name):
+      self.name = name
+      self.ledger = list()
 
-chart += "    " + "-"*10 + ' \n'
+  def __str__(self):
+      title = f"{self.name:*^30}\n"
+      items = ""
+      total = 0
+      for item in self.ledger:
+          items += f"{item['description'][0:23]:23}" + f"{item['amount']:>7.2f}" + '\n'
+          
+          total += item['amount']
 
-for name in zip(*padded):
-    # print(' '.join(name),end="\n")
-    chart += "     "
-    chart += ('  '.join(name)) + '  \n'
+      output = title + items + "Total: " + str(total)
+      return output
 
-print(chart.rstrip('\n'))
+  def deposit(self, amount, description=""):
+      self.ledger.append({"amount": amount, "description": description})
+
+
+  def withdraw(self, amount, description=""):
+      if(self.check_funds(amount)):
+        self.ledger.append({"amount": -amount, "description": description})
+        return True
+      return False
+
+
+  def get_balance(self):
+      total_cash = 0
+      for item in self.ledger:
+        total_cash += item["amount"]
+
+      return total_cash
+
+
+  def transfer(self, amount, category):
+      if(self.check_funds(amount)):
+        self.withdraw(amount,"Transfer to " + category.name)
+        category.deposit(amount, "Transfer from " + self.name)
+        return True
+      return False
+      
+
+  def check_funds(self, amount):
+      if(self.get_balance() >= amount):
+        return True
+      return False
+      
+  ### Category method
+  def get_withdrawls(self):
+      total = 0
+      for item in self.ledger:
+          if item["amount"] < 0:
+              total+= item["amount"]
+      return total
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def truncate(n):
+    multiplier = 10
+    return int(n * multiplier) / multiplier
+
+def getTotals(categories):
+    total = 0
+    breakdown = []
+    for category in categories:
+        total += category.get_withdrawls()
+        breakdown.append(category.get_withdrawls())
+    rounded = list(map(lambda x: truncate(x/total), breakdown))
+    return rounded
+
+def create_spend_chart(categories):
+    """
+    create_spend_chart that takes a list of categories as an argument. It should return a string that is a bar chart
+    """
+    res = "Percentage spent by category\n"
+    i = 100
+    totals = getTotals(categories)
+    while i >= 0:
+          cat_spaces = " "
+          for total in totals:
+              if total * 100 >= i:
+                  cat_spaces += "o  "
+              else:
+                  cat_spaces += "   "
+          res+= str(i).rjust(3) + "|" + cat_spaces + ("\n")
+          i-=10
+      
+    dashes = "-" + "---"*len(categories)
+    names = []
+    x_axis = ""
+    for category in categories:
+          names.append(category.name)
+
+    maxi = max(names, key=len)
+
+    for x in range(len(maxi)):
+        nameStr = '     '
+        for name in names:
+              if x >= len(name):
+                  nameStr += "   "
+              else:
+                  nameStr += name[x] + "  "
+        
+        if(x != len(maxi) -1 ):
+          nameStr += '\n'
+
+          
+        x_axis += nameStr
+
+    res+= dashes.rjust(len(dashes)+4) + "\n" + x_axis
+    return res
+
+
+
+
