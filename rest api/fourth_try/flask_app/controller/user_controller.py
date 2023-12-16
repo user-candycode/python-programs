9e9d5a2f860655822dcda84119a0014b3b573748
candy <user.candycode@gmail.com>
2023-12-16 12:21:39 +0530

    pagination and crud operations done

--- rest api/fourth_try/flask_app/controller/user_controller.py
+++ rest api/fourth_try/flask_app/controller/user_controller.py
@@ -20,3 +20,11 @@ def user_update_controller():
 @app.route("/user/delete/<id>", methods=["DELETE"])
 def user_delete_controller(id):
     return obj.user_delete_controller(id)
+
+@app.route("/user/patch/<id>", methods=["PATCH"])
+def user_patch_controller(id):
+    return obj.user_patch_model(request.form, id)
+
+@app.route("/user/getall/limit/<limit>/page/<page>", methods=["GET"])
+def user_pagination_controller(limit,page):
+    return obj.user_pagination_model(limit, page)
--- rest api/fourth_try/flask_app/model/user_model.py
+++ rest api/fourth_try/flask_app/model/user_model.py
@@ -1,7 +1,8 @@
 import mysql.connector
 import json
+from flask import make_response
 class User_model:
-     def __init__(self):
+    def __init__(self):
             hostname = "localhost"
             username = "candy"
             passwd = "root"
@@ -14,32 +15,61 @@ class User_model:
                 print("Connection sucessfull to mysql")
             except:
                 print("Some Error")
-     def user_getall_model(self):
+    def user_getall_model(self):
         # business logic
         self.cur.execute("SELECT * FROM users;")
         result = self.cur.fetchall()
         if len(result)>0:
-          #    return json.dumps(result)
-            return {"payload": result}
+            res = make_response({"payload": result}, 200)
+            res.headers['Access-Control-Allow-Origin'] = "*"
+            return res
         else:
-             return {"message":"No data found"}
+             return make_response({"message":"No data found"}, 204)
         
-     def user_addone_model(self,data):
+    def user_addone_model(self,data):
          self.cur.execute(f"INSERT INTO users(name,email,phone,role,password) VALUES('{data['name']}','{data['email']}','{data['phone']}','{data['role']}','{data['password']}')")
-         return {"message":"User Created Sucessfully"}
+         return make_response({"message":"User Created Sucessfully"}, 201)
     
-     def user_update_controller(self,data):
+    def user_update_controller(self,data):
          self.cur.execute(f"UPDATE users SET name='{data['name']}',email='{data['email']}',phone='{data['phone']}',role='{data['role']}',password='{data['password']}' WHERE id={data['id']}")
          
          if self.cur.rowcount>0:
-            return {"message":"USer Updated Sucessfully"}
+            return make_response({"message":"USer Updated Sucessfully"}, 201)
          else:
-              return {"message":"Didnt updated anything"}
+              return make_response({"message":"Didnt updated anything"}, 202)
               
-     def user_delete_controller(self,id):
+    def user_delete_controller(self,id):
          self.cur.execute(f"DELETE FROM users WHERE id={id}")
          if self.cur.rowcount>0:
-            return "User DELETED Sucessfully"
+            return make_response({"message":"User DELETED Sucessfully"}, 200)
          else:
-              return "Nothing to delete"
+              return make_response({"message":"Nothing to delete"}, 202)
          
+    def user_patch_model(self,data,id):
+        # Update users SET col=val ,col2=val2 WHERE id={id}
+        qry ="UPDATE users SET "
+        for key in data:
+            qry +=f"{key}='{data[key]}',"
+        qry = qry[:-1]
+        qry += f" WHERE id={id};"
+
+        self.cur.execute(qry)
+        if self.cur.rowcount>0:
+            return make_response({"message":"USer patched Sucessfully"}, 201)
+        else:
+              return make_response({"message":"Didnt patched anything"}, 202)
+    
+    def user_pagination_model(self,limit,page):
+        limit = (int(limit))
+        page = int(page)
+        total = int(page*limit)
+        start = total - limit
+        qry = f"SELECT * FROM users LIMIT {start},{limit};"
+
+        self.cur.execute(qry)
+        result = self.cur.fetchall()
+        if len(result)>0:
+            res = make_response({"payload": result, "page_no":page, "limit":limit}, 200)
+            return res
+        else:
+             return make_response({"message":"No data found"}, 204)
